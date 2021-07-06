ae7b4f901fe8c4cf5c1ddee6140406e3f49e194a
myriahnottage <myriah.nottage@gmail.com>
2021-07-06 15:35:50 -0700

    take home

---
+++ day-02/js/dailyChallenge.js
@@ -0,0 +1,8 @@
+const string = "Supercalifragilisticexpialidocious";
+console.log(string.length);
+
+const anyNumber = 1 +2;
+console.log(anyNumber);
+
+const bigNumber = BigInt(2 ** 53);
+console.log(bigNumber);
--- day-02/js/takeHome.js
+++ day-02/js/takeHome.js
@@ -1,8 +1,84 @@
-const string = "Supercalifragilisticexpialidocious";
-console.log(string.length);
+// Assign STRING VALUES ONLY to all of the following variables.
+// Assign an adjective
+let adj1 = "big";
+// Assign an adjective
+let adj2 = "gracious";
+// Assign an adjective
+let adj3 = "bubbly";
+// Assign an adjective
+let adj4 = "curvaceous";
+// Assign an adjective
+let adj5 = "shady";
+// Assign an adjective
+let adj6 = "papery";
+// Assign an adverb
+let adverb = "quickly";
+// Assign a color
+let color = 'tomato';
+// Assign a noun
+let noun1 = "cat";
+// Assign a noun
+let noun2 = "crate";
+// Assign a noun
+let noun3 = "cup";
+// Assign a noun
+let noun4 = "charlatan";
+// Assign a noun
+let noun5 = "Chicago";
+// Assign a noun
+let noun6 = "couch";
+// Assign a number
+let num = "25";
+// Assign a plural noun
+let pnoun1 = "cats";
 
-const anyNumber = 1 +2;
-console.log(anyNumber);
+// Open your console to see the results!
+// SPOILER ALERT: KEEP SCROLLING TO VIEW THE REST OF THE CODE
 
-const bigNumber = BigInt(2 ** 53);
-console.log(bigNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Using template literals to plug in the values
+let story = `It has often been said that a dog is a man's best ${noun1}. Dogs are very ${adj1} and can be taught many ${adj2}
+tricks. A dog can be trained to carry a ${noun2} in his mouth.
+If you throw his ${noun3}, he will run and fetch it. Dogs
+will also bark ${adverb} if someone tries to break into your
+${noun4} during the night. One of the most popular canine pets
+today is the ${noun5} Spaniel. Spaniels have curly ${color}
+coats and ${adj3} ears. They also have very ${adj4}
+dispositions and live to be ${num} years old. Other popular dogs
+are ${adj5} Terriers, German ${pnoun1}, and the
+${adj6} Poodle. Every ${noun6} should have a loyal dog.`;
+
+//Console log the completed story
+console.log(story);
